3beee39e0e776ff98a0b9d878298770c670ca3c8
jolo-v <jolo_villanueva@yahoo.com>
2025-05-21 13:40:24 +0800

    upload files to GitHub

---
+++ streamlit_chat_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_query_functions import query
+
+load_dotenv()
+
+#Page title
+st.title("Vienna Conventions Chat App")
+chat_placeholder = st.empty()
+
+#Initilaize chat history with system message
+def init_chat_history():
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+        st.session_state.messages = [{"role":"system","content":"You are a helpful assitant."}]
+
+#Start the chat
+def start_chat():
+    #Display chat messages from history
+    with chat_placeholder.container():
+        for message in st.session_state.messages:
+            if message["role"] != "system":
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+    #Accept user input
+    if prompt := st.chat_input("This chatbot is here to answer your questions about the Vienna Conventions of 1961, 1963, and 1969."):
+        #Add message to chat history
+        st.session_state.messages.append({"role":"user","content":prompt})
+        #Display user message in chat container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        #Generate response from LLM
+        response = query(prompt)
+        with st.chat_message("assistant"):
+            st.markdown(response["answer"])
+        #Add LLM's response to chat history
+        st.session_state.messages.append({"role":"assistant","content":response["answer"]})
+
+if __name__ == "__main__":
+    init_chat_history()
+    start_chat()
+
+
---
+++ streamlit_query_functions.py
@@ -0,0 +1,82 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from langchain_mongodb.vectorstores import MongoDBAtlasVectorSearch
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain.chat_models import init_chat_model
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+#vector db details
+ATLAS_CONNECTION_STRING = os.getenv("ATLAS_CONNECTION_STRING")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+ATLAS_VECTOR_SEARCH_INDEX_NAME = os.getenv("ATLAS_VECTOR_SEARCH_INDEX_NAME")
+
+#Instantiate vector store
+embeddings = VertexAIEmbeddings('text-embedding-005')
+namespace = (DB_NAME + "." + COLLECTION_NAME)
+
+vector_store = MongoDBAtlasVectorSearch.from_connection_string(
+  connection_string = ATLAS_CONNECTION_STRING,
+  namespace = namespace,
+  embedding = embeddings,
+  index_name = ATLAS_VECTOR_SEARCH_INDEX_NAME
+)
+
+#Define model
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+#Initialize chat history
+chat_history = []
+
+#Configure prompt for system to use to reformulate original prompt after processing chat history
+chathist_system_prompt = """Given a chat history and the latest user question which  might reference context in the chat history, formulate a standalone question which can be understood without the chat history. Do NOT answer the question, just reformulate it if needed and otherwise return it as is."""
+chathist_prompt = ChatPromptTemplate.from_messages(
+    [("system",chathist_system_prompt),
+     MessagesPlaceholder("chat_history"),
+     ("human","{input}")
+     ]
+     )
+
+#Configure main question and answer prompt
+qa_system_prompt = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+{context} 
+"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [("system",qa_system_prompt),
+     MessagesPlaceholder("chat_history"),
+     ("human","{input}")
+     ]
+     )
+
+#Define vector store as retriever
+retriever = vector_store.as_retriever(search_type='similarity')
+
+#Define chat history chain
+history_aware_retriever = create_history_aware_retriever(model, retriever, chathist_prompt)
+
+#Define main q&a chain
+qa_chain = create_stuff_documents_chain(model, qa_prompt)
+
+
+#Define the main RAG chain
+def rag_response(query):
+    rag_chain = create_retrieval_chain(history_aware_retriever,qa_chain)
+    return rag_chain.invoke({"input":query,
+                             "chat_history":chat_history})
+
+#Define function that will execute RAG chain and record chat history
+def query(query):
+    response = rag_response(query)
+    chat_history.extend([HumanMessage(content=query), response["answer"]])
+    print("history", response["chat_history"])
+    return response
+
+
+
+
